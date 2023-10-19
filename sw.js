0904eadb8698a3b4482bbe92d375175186207712
jimconsuelo21 <123600895+jimconsuelo21@users.noreply.github.com>
2023-10-18 18:02:37 -0600

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,61 @@
+importScripts('js/sw-utils.js');
+const STATIC_CACHE = 'static-v3';
+const DYNAMIC_CACHE = 'dynamic-v1';
+const INMUTABLE_CACHE = 'inmutable-v1';
+
+const APP_SHELL = [
+    '/',
+    'index.html',
+    'css/style.css',
+    'img/favicon.ico',
+    'img/avatars/hulk.jpg',
+    'img/avatars/ironman.jpg',
+    'img/avatars/spiderman.jpg',
+    'img/avatars/thor.jpg',
+    'img/avatars/wolverine.jpg',
+    'js/app.js',
+    'js/sw-utils.js'
+];
+
+const APP_SHELL_INMUTABLE = [
+   'https://fonts.googleapis.com/css?family=Quicksand:300,400',
+	'https://fonts.googleapis.com/css?family=Lato:400,300',
+	'https://use.fontawesome.com/releases/v5.3.1/css/all.css',
+    'css/animate.css',
+    'js/libs/jquery.js'
+];
+
+self.addEventListener('install',e =>{ //variable asignada es la e 
+    const cacheStatic = caches.open(STATIC_CACHE).then(cache=>
+        cache.addAll(APP_SHELL));
+        const cacheInmutable = caches.open(INMUTABLE_CACHE).then(cache=>
+            cache.addAll(APP_SHELL_INMUTABLE));
+
+            e.waitUntil(Promise.all([cacheStatic,cacheInmutable]));
+
+});
+
+self.addEventListener('activate',e=>{ //Borra las caches viejas  
+    const respuesta = caches.keys().then(keys=>{// alistar las caches 
+        keys.forEach(key=>{
+            if (key!= STATIC_CACHE && key.includes('static')){ // si la cache es diferente a static_cache esta en uno 
+                return caches.delete(key);
+            }
+        });
+    });
+        e.waitUntil(respuesta);
+    });
+
+self.addEventListener('fetch',e =>{
+    const respuesta =caches.match(e.request).then(resp=>{ //estrategia cache onli 
+        if(resp){
+            return resp;   
+        }else{
+            return fetch(e.request).then(newResp=>{
+                return actualizaCacheDinamico(DYNAMIC_CACHE,e.request,newResp);
+            });   
+           // console.log(e.request.url);//permite responder a la solicitud con lo guardado en el cache
+        }
+    });
+    e.respondWith(respuesta);
+});
